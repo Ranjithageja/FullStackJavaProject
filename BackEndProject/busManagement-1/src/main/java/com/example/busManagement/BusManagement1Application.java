68471cace70b0a8f98193990a061fdacae6e6be6
Ranjithageja <120001421+Ranjithageja@users.noreply.github.com>
2023-03-09 12:32:34 +0530

    Add files via upload

---
+++ BackEndProject/busManagement-1/src/main/java/com/example/busManagement/BusManagement1Application.java
@@ -0,0 +1,13 @@
+package com.example.busManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusManagement1Application.class, args);
+	}
+
+}
---
+++ BackEndProject/busManagement-1/src/main/java/com/example/busManagement/repository/BusManagementRepository.java
@@ -0,0 +1,11 @@
+package com.example.busManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.busManagement.model.BusManagement;
+
+public interface BusManagementRepository extends JpaRepository<BusManagement, Integer>{
+ 
+
+ 
+}
