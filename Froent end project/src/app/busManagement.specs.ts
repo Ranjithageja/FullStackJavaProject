76b191b55aaf537d60ac944d6563e75ae9a2498f
Ranjithageja <120001421+Ranjithageja@users.noreply.github.com>
2023-04-24 13:45:53 +0530

    Delete Froent end project/src directory

--- Froent end project/src/app/busManagement.specs.ts
+++
@@ -1,7 +0,0 @@
-import { BusManagement } from './busManagement';
-
-describe('BusManagement', () => {
-  it('should create an instance', () => {
-    expect(new BusManagement()).toBeTruthy();
-  });
-});
